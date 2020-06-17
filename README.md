e4a1f659ae963c1fb118dfa0ef15fa450a41d536
XJTUzhangliwen <liwen_zhang@stu.xjtu.edu.cn>
2020-06-17 11:39:54 +0800

    update readme

--- README.md
+++ README.md
@@ -5,10 +5,47 @@ Code and datasets for article ''EC-Clustering: A Federated Clustering Algorithm 
 - NumPy (>= 1.13.3)
 - SciPy (>= 0.19.1)
 - Sklearn (>= 0.11)
-## Datasets
+## Datasets in DATA
+Dataset | Filename  
+-|-|
+IID A3 dataset | A3_iid_$edge_number$.csv |
+non-IID A3 dataset | A3_noniid_$edge_number$.csv |
+IID ImagenetS dataset | ImagenetS_iid_$edge_number$.csv |
+non-IID ImagenetS dataset | ImagenetS_noniid_$edge_number$.csv |
 
- | Dataset                          | File Name                     |
- | IID partitioned A3 dataset       | A3_iid_$edge_number$.csv      |
- | non-IID partitioned A3 dataset   | A3_noniid_$edge_number$.csv   |
- | IID partitioned ImagenetS dataset       | ImagenetS_iid_$edge_number$.csv      |
- | non-IID partitioned ImagenetS dataset   | ImagenetS_noniid_$edge_number$.csv   |
+2D dataset [A3](http://cs.uef.fi/sipu/datasets/a3.txt) —— a synthetic two-dimensional dataset with 50 circular clusters and 150 points for each cluster.
+
+High-dimension dataset [ImagenetS](http://image-net.org/challenges/LSVRC/2012/) —— 4,096-dimension features extracted from images of top ten classes in ILSVRC2012-validation by VGG16(fs_conv5_2 layer).
+## Example of EC-Clustering 
+Here, we give an example for running EC-Clustering on A3 IID datasets.
+
+Firstly, initialize the parameters for clustering task.
+
+```
+input_path = r'.\DATA\A3_iid_%d.csv'
+input_lable = r'.\DATA\A3_iid_%d_label.csv'
+a3_delta = 0.001
+a3_clusters = 50
+```
+Then, load local dataset for initiating each edge
+```
+data = np.loadtxt(input_path%(e_i))
+edge = edge_ECClustering(X = data, n_clusters = a3_clusters, delta = a3_delta)
+```
+Execute edge summarization in each edge, and receive the summary of local dataset.
+```
+summary = edge.edge_summarize()
+```
+Upload summary of edges to the cloud and execute cloud clustering.
+```
+cloud = cloud_ECClustering(n_clusters = a3_clusters)
+cloud_centers = cloud.cloud_clustering(edge_summary)
+```
+Broadcast cloud clustering results to each edge.
+```
+for edge in edge_list:
+    edge.deliverLabel_allcenter(cloud_centers)
+```
+We also give a f1-score measurement function `f1()` to envalue the clustering results.
+
+
--- ec_clustering.py
+++ ec_clustering.py
@@ -1,7 +1,31 @@
 from utils import *
 
 class edge_ECClustering:
+    ''' Edge functions for EC-Clustering algorithm
+    Functions:
+    -----------------------------------------------
+        __init__ - Initiate edge object
+
+        edge_summarize - Function for edge summarization
+
+        deliverLabel_allcenter - Receive centers from cloud and recalculate the belonging of points in edge 
+    Attributs:
+    -----------------------------------------------
+        X - (np.array) local dataset for edge
+        delta - (float) parameter to estimate cluster number in X
+        n_clusters - (int) parameter to limit maximum uploading cluster number
+        centers - k-means cluster centers generated from X or sent by cloud
+        mass - sum of distence from points in X to the closest centers
+        labels - clustering results    
+    '''
     def __init__(self, **kwargs):
+        ''' Initiate edge object
+        Parameters:
+        -------------------------------------------
+            X - (np.array) local dataset for edge
+            delta - (float) parameter to estimate cluster number in X
+            n_clusters - (int) parameter to limit maximum uploading cluster number
+        '''
         if 'X' in kwargs:
             self.X = kwargs.get('X')
         if 'delta' in kwargs:
@@ -9,11 +33,18 @@ class edge_ECClustering:
         if 'n_clusters' in kwargs:
             self.n_clusters = kwargs.get('n_clusters')
     def edge_summarize(self):
+        ''' Function for edge summarization
+        --------------------------------------------
+            Details in utils.edge_summerize
+        Return:
+        --------------------------------------------
+            centers - (list of k_means_center) k-means cluster centers generated from X
+        '''
         centers,labels = edge_summarize(self.X, self.n_clusters*100, self.delta, copy_data = False)
         self.centers = centers
         return centers
     def deliverLabel_allcenter(self, centers):
-        ''' receive centers from cloud and recalculate the belonging of points in edge 
+        ''' Receive centers from cloud and recalculate the belonging of points in edge 
         Parameters:
         ----------------------------------
             centers - (np.array) recieved from cloud for labeling dataset in edge server
@@ -30,9 +61,34 @@ class edge_ECClustering:
         self.mass = mass
 
 class cloud_ECClustering:
+    ''' Cloud functions for EC-Clustering algorithm
+    Functions:
+    -----------------------------------------------
+        __init__ - Initiate cloud object
+
+        cloud_clustering - Function for cloud clustering
+    Attributs:
+    -----------------------------------------------
+        n_clusters - (int) parameter of k-means set by users
+        centers - (list of edge_center )k-means cluster centers for broadcasting   
+    '''
     def __init__(self, **kwargs):
+        ''' Initiate edge object
+        Parameters:
+        -------------------------------------------
+            n_clusters - (int) parameter of k-means set by users
+        '''
         if 'n_clusters' in kwargs:
             self.n_clusters = kwargs.get('n_clusters')
-    def cloud_clustering(self, centers):
-        self.centers = cloud_clustering(centers, self.n_clusters)
-        return self.centers
+    def cloud_clustering(self, in_centers):
+        ''' Function for edge summarization
+        Parameter:
+        --------------------------------------------
+            in_centers - (list of k_means_center objects) uploading by edges
+        Return:
+        --------------------------------------------
+            out_centers - (list of edge_centers) broacast centers
+        '''
+        out_centers = cloud_clustering(centers, self.n_clusters)
+        self.centers = out_centers
+        return out_centers
